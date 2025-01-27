6bbb4ec86d48166a630e1dc4296959fa8b8804c5
Ripponcheese <hayooncode@gmail.com>
2025-01-27 21:55:05 +0900

    init: app.js에 dotenv파일로드, prisma 연결테스트 추가

--- prisma/app.js
+++ prisma/app.js
@@ -1,12 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import router from "./router";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const PORT = process.env.PORT || 3000;
 
