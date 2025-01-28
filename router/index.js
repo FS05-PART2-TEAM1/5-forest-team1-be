8fd4e4efde3371bbb2a770e396df9d99aa206116
Ripponcheese <hayooncode@gmail.com>
2025-01-28 17:17:11 +0900

    init project

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import studyRouter from "./study";
+import studyRouter from "./studies";
 
 router.use("/studies", studyRouter);
 
