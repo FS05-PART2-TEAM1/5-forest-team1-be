0590c65c3c3635b91249f5b9dcdb3a07996e0879
Ripponcheese <hayooncode@gmail.com>
2025-01-28 16:36:27 +0900

    init project

--- router/point/index.js
+++ router/point/index.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { getStudyPoints, createPoint } from "./point.controller.js";
 
 const router = express.Router();
 
-router.get("/");
-router.post("/");
+router.get("/:studyId/points", getStudyPoints);
+router.post("/:studyId/points", createPoint);
 
 export default router;
