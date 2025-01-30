2c46a1463234a6a02ba81f04edbf49c176c44fd1
hyuk-dev <hyuk.development@gmail.com>
2025-01-30 22:09:06 +0900

    fix: index.js 내에서 deleteHabit 이라고 사용했었던 부분 지워지지 않아서 제거

--- router/habits/index.js
+++ router/habits/index.js
@@ -1,17 +1,16 @@
-import express from "express";
+import express from 'express';
 import {
   getHabits,
   createHabit,
-  deleteHabit,
   updateHabit,
   removeHabitById,
-} from "./habit.controller.js";
+} from './habit.controller.js';
 
 const router = express.Router();
 
-router.get("/", getHabits);
-router.post("/", createHabit);
-router.delete("/:habitId", removeHabitById);
-router.patch("/:habitId", updateHabit);
+router.get('/', getHabits);
+router.post('/', createHabit);
+router.delete('/:habitId', removeHabitById);
+router.patch('/:habitId', updateHabit);
 
 export default router;
