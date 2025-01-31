689575c923d5c4a2e2ad76f5f3a6b97cf8202850
Syyga <gsh2672@gmail.com>
2025-02-01 00:21:53 +0900

    feat/스터디 수정 서비스 생성

--- router/studies/study.controller.js
+++ router/studies/study.controller.js
@@ -15,3 +15,29 @@ export const fetchAllStudies = async (req, res) => {
     res.status(500).send({ error: "스터디 목록을 가져오는데 실패했습니다." });
   }
 };
+
+/// 스터디 수정 API
+export const uapdateStudy = async (req, res) => {
+  const { studyId } = req.params;
+  const { name, password, description, backgroundIrl, points } = req.body;
+
+  try {
+    const result = await studyService.updateStudy(
+      studyId,
+      name,
+      description,
+      backgroundIrl
+    );
+
+    if (!result) {
+      return res.status(404).send({ error: "스터디를 찾을 수 없습니다." });
+    }
+
+    return res
+      .status(200)
+      .send({ message: "스터디가 성공적으로 수정되었습니다.", study: result });
+  } catch (err) {
+    console.error("스터디 수정 중 오류 발생", err);
+    return res.status(500).send({ error: "스터디 수정에 실패했습니다." });
+  }
+};
