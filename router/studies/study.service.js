749331a35cae5b99d7efb3f15faaf49355513e9b
Ripponcheese <hayooncode@gmail.com>
2025-02-01 17:46:18 +0900

    refactor: 불필요한 로직 제거

--- router/studies/study.service.js
+++ router/studies/study.service.js
@@ -57,26 +57,8 @@ export const addStudy = async (studyData) => {
   const { name, password, passwordConfirm, description, backgroundImageUrl } =
     studyData;
 
-  // 비밀번호 일치 여부 검증
   if (password !== passwordConfirm) {
     throw new Error("비밀번호가 일치하지 않습니다.");
-  }
-
-  // 동일한 스터디 이름이 있는지 확인
-  const existingStudy = await prisma.study.findFirst({
-    // name 필드가 unique로 설정되어있지 않아 findUnique대신 사용
-    where: {
-      name: name,
-    },
-  });
-
-  // 이미 존재하는 스터디인 경우
-  if (existingStudy) {
-    // 비밀번호 검증
-    if (existingStudy.password !== password) {
-      throw new Error("비밀번호가 일치하지 않습니다.");
-    }
-    throw new Error("이미 존재하는 스터디 이름입니다.");
   }
 
   const study = await prisma.study.create({
