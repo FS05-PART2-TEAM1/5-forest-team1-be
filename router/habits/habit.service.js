982a97194a27e36a45991cfd4c795999932ab8f0
hyuk-dev <hyuk.development@gmail.com>
2025-02-03 10:41:42 +0900

    recommit

--- router/habits/habit.service.js
+++ router/habits/habit.service.js
@@ -1,19 +1,18 @@
 import prisma from '../../prismaClient.js';
 
 const modifyHabitById = async (habitId, data) => {
-    const updatedHabit = await prisma.habit.update({
-        where: {
-            id: habitId,
-        },
-        data,
-        select: { habitId: true, deletedAt: true, select: true },
-    })
-    return updatedHabit;
-}
-
+  const updatedHabit = await prisma.habit.update({
+    where: {
+      id: habitId,
+    },
+    data,
+    select: { habitId: true, deletedAt: true, select: true },
+  });
+  return updatedHabit;
+};
 
 const habitService = {
-    modifyHabitById,
-}
+  modifyHabitById,
+};
 
 export default habitService;
