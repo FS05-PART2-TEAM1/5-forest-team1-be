06ba2dfcb2c5fc4e27666ce4999271be9937e477
Syyga <gsh2672@gmail.com>
2025-02-03 09:28:15 +0900

    Feat:스터디 삭제 API 추가

--- router/studies/study.controller.js
+++ router/studies/study.controller.js
@@ -15,3 +15,25 @@ export const fetchAllStudies = async (req, res) => {
     res.status(500).send({ error: "스터디 목록을 가져오는데 실패했습니다." });
   }
 };
+
+/// 스터디 삭제
+export const deleteStudy = async (req, res) => {
+  const { studyId } = req.params;
+
+  try {
+    const deleted = await studyService.deleteStudy(studyId);
+
+    if (!deleted) {
+      return res.status(404).send({ error: "해당 스터디를 찾을 수 없습니다." });
+    }
+    return res.status(204).send({
+      message: "스터디가 삭제되었습니다.",
+      study: deleted,
+    });
+  } catch (err) {
+    console.error("삭제 중 오류 발생", err);
+    return res
+      .status(500)
+      .send({ error: "스터디 삭제 중 오류가 발생했습니다." });
+  }
+};
