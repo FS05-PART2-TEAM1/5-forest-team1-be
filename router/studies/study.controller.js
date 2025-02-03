f0a6fac56fe9c7abcfdf348a5a88f685aee6d550
Syyga <gsh2672@gmail.com>
2025-02-03 09:54:57 +0900

    fix:스터디 수정 시 필드에  password 출력 미포함

--- router/studies/study.controller.js
+++ router/studies/study.controller.js
@@ -32,10 +32,12 @@ export const updateStudy = async (req, res) => {
     if (!result) {
       return res.status(404).send({ error: "스터디를 찾을 수 없습니다." });
     }
+    const { password, ...studyWithoutPassword } = result;
 
-    return res
-      .status(200)
-      .send({ message: "스터디가 성공적으로 수정되었습니다.", study: result });
+    return res.status(200).send({
+      message: "스터디가 성공적으로 수정되었습니다.",
+      study: studyWithoutPassword,
+    });
   } catch (err) {
     console.error("스터디 수정 중 오류 발생", err);
     return res.status(500).send({ error: "스터디 수정에 실패했습니다." });
