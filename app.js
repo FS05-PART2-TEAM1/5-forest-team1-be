79580a32fd6c6500ed2e52227f576a6f74e97f93
silverraining <www.stella.choi@gmail.com>
2025-02-04 21:06:00 -0800

    chore: 프리즈마 마이그레이션 적용, favicon추가 및 .gitignore업데이트

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ import dotenv from "dotenv";
 import cors from "cors";
 import router from "./router/index.js";
 import prisma from "./prismaClient.js";
-
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://127.0.0.1:5174",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+  ],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+// favicon.ico 처리: 정적 파일 제공
+app.use(express.static(path.join(__dirname)));
+
+// /favicon.ico 요청이 들어오면 이를 처리
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile(path.join(__dirname, "favicon.ico"));
+});
+
 app.use("/api", router);
 
 prisma
