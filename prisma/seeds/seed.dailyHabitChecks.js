99357c0e1c74d6db1f5fda52127ad470e7ed1b67
hyuk-dev <hyuk.development@gmail.com>
2025-02-07 08:17:45 +0900

    fix: seed 수정

--- prisma/seeds/seed.dailyHabitChecks.js
+++ prisma/seeds/seed.dailyHabitChecks.js
@@ -1,37 +1,30 @@
 export const dailyHabitChecks = [
   {
     habitId: 'test',
-    habitName: '밥먹기',
     date: new Date('2025-02-05'),
   },
   {
     habitId: 'test',
-    habitName: '밥먹기',
     date: new Date('2025-02-04'),
   },
   {
     habitId: 'test',
-    habitName: '밥먹기',
     date: new Date('2025-02-03'),
   },
   {
     habitId: 'test',
-    habitName: '밥먹기',
     date: new Date('2025-02-06'),
   },
   {
     habitId: 'test',
-    habitName: '밥먹기',
     date: new Date('2025-02-07'),
   },
   {
     habitId: 'test',
-    habitName: '밥먹기',
     date: new Date('2025-02-08'),
   },
   {
     habitId: 'hello',
-    habitName: '인사하기',
     date: new Date('2025-02-04'),
   },
 ];
